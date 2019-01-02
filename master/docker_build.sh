58f66e152d7532d16f1fd51bb8786b9d4ae385e3
Remco Perlee <remco.perlee@gmail.com>
2019-01-02 21:12:00 +0100

    temporarily with cache due to performance of build

--- master/docker_build.sh
+++ master/docker_build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-microk8s.docker build --no-cache=true -t 127.0.0.1:32000/jenkins-master .
+microk8s.docker build -t 127.0.0.1:32000/jenkins-master .
+# --no-cache=true
 microk8s.docker push 127.0.0.1:32000/jenkins-master
