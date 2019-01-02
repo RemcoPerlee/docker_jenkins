20ea494c5f6140f8e151cd7397efca8f951b20c8
Remco Perlee <remco.perlee@gmail.com>
2019-01-02 21:10:33 +0100

    step 2: master in kubernetes

--- master/rebuild.sh
+++ master/rebuild.sh
@@ -1,5 +1,5 @@
-# kubectl delete pod -l app=couchpotato --namespace=download
-#
-# kubectl apply -f namespace.yaml
-# kubectl apply -f deployment.yaml --namespace=download
-# kubectl apply -f service.yaml    --namespace=download
+kubectl delete pod -l app=jenkins-master --namespace=jenkins
+
+kubectl apply -f namespace.yaml
+kubectl apply -f deployment.yaml --namespace=jenkins
+kubectl apply -f service.yaml    --namespace=jenkins
