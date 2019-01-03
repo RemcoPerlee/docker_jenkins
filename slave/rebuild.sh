9f3c3bc4213a321b12573d127baa21b511f29aec
Remco Perlee <remco.perlee@gmail.com>
2019-01-03 22:32:25 +0100

    no message

--- slave/rebuild.sh
+++ slave/rebuild.sh
@@ -1,4 +1,4 @@
-# kubectl delete pod -l app=couchpotato --namespace=download
+kubectl delete pod -l jenkins=slave --namespace=jenkins
 #
 # kubectl apply -f namespace.yaml
 # kubectl apply -f deployment.yaml --namespace=download
